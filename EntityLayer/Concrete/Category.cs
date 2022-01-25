fb94689934d4c371b29ffe9bcb36a0d6a38d2ebb
ezgibagcik <ezgibagcik@hotmail.com>
2022-01-25 23:53:11 +0300

    First commit

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [StringLength(50)]
+        public string CategoryName  { get; set; }
+        [StringLength(200)]
+        public string CategoyDescription  { get; set; }
+        public bool CategoryStatus  { get; set; } //durumu pasif-aktif hale getirmek
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class Content
+    {
+        [Key]
+        public int ContentId { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; } //içeriği
+        public DateTime ContentDate { get; set; }
+
+        //Content-Heading İlişkisi
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        //Content Yazar İlişkisi
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+    }
+}
